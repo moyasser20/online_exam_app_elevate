e239ab4844a91929ecb4fb2b990158f3658dfad2
moyasser20 <mohamedyasser192023@gmail.com>
2025-07-11 18:24:12 +0300

    core of the application #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:online_exam_app_elevate/core/di/di.dart';
 
 
 import 'core/l10n/translation/app_localizations.dart';
@@ -9,6 +10,7 @@ import 'core/theme/app_theme.dart';
 
 void main()
 {
+  configureDependencies();
   runApp(MyApp());
 }
 
