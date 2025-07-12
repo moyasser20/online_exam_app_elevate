af3b46cd53171b1d29c78a718e4cf246f6f8268a
moyasser20 <mohamedyasser192023@gmail.com>
2025-07-13 02:09:50 +0300

    adding end point of the api at constant

---
+++ lib/core/constants/api_constants/end_points.dart
@@ -0,0 +1,13 @@
+abstract class EndPoints {
+  static const String signIn = "auth/signin";
+  static const String signup = "auth/signup";
+  static const String changePassword = "auth/changePassword";
+  static const String deleteMe = "auth/deleteMe";
+  static const String editMyProfile = "auth/editProfile";
+  static const String logout = "auth/logout";
+  static const String profileData = "auth/profileData";
+  static const String forgetPassword = "auth/forgotPassword";
+  static const String verifyResetCode = "auth/verifyResetCode";
+  static const String resetPassword = "auth/resetPassword";
+
+}
